f2f16dd3cc491aa172bf01bd85a8539ca5c18582
davidrose07 <davidrose071783@gmail.com>
2025-06-20 19:58:25 -0500

    Windows Support

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
+def load_requirements(filename):
+    with open(filename, encoding='utf-8') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+    
 setup(
     name="bible-gui",
     version="1.0",
     description="Read the Bible in a Gui Interface",
     author="David Rose",
-    author_email="",
-    url="",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=load_requirements('requirements.txt'),
     entry_points={"console_scripts": ["bible-gui=src.main:main"]},
-    install_requires=[],
-    license="",
+    license="MIT",
 )
